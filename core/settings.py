824258583cf1c48cc25e1b773624f9c77487dd9e
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-08-07 14:07:31 +0500

    tests updated

--- core/settings.py
+++ core/settings.py
@@ -110,10 +110,10 @@ DATABASES = {
     'default': {
         'ENGINE': config('DB_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': config('DB_NAME', default='db.sqlite3'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'USER': config('DB_USER', ''),
+        'PASSWORD': config('DB_PASSWORD', ''),
+        'HOST': config('DB_HOST', ''),
+        'PORT': config('DB_PORT', ''),
     }
 }
 
